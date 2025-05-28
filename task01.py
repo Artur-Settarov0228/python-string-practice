11c8460de25b31c40a9da3f39ade8c6ac0bd2d2e
settarovartur <settarovartur2@gamil.com>
2025-05-28 10:08:18 +0500

    bajarildi

--- task01.py
+++ task01.py
@@ -1,1 +1,7 @@
-
+raqam = input("raqam kiriting :")
+raqamlar = raqam[0:1]
+raqamlar2 =raqam[2:5]
+raqamlar3 = raqam[5:7]
+raqamlar4 = raqam[7:]
+formatlash = f"+998 {raqamlar} {raqamlar2} {raqamlar3} {raqamlar4}"
+print(formatlash)

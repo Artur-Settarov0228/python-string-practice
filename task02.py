95d131dc315f0e87a20353e14c5efa334202bd7f
settarovartur <settarovartur2@gamil.com>
2025-05-28 10:08:38 +0500

    bajarldi

--- task02.py
+++ task02.py
@@ -1,1 +1,9 @@
+import random
 
+ism = input("ism kiriting :")
+familya = input('familyani kiriting :')
+
+print(ism + "." + familya)
+print(ism + "_" + familya)
+print(familya + str(random.randint(100, 999)))
+print(ism + str(len(ism + familya)))
--- task03.py
+++ task03.py
@@ -1,1 +1,5 @@
-
+email = input("emailizni kiriting  :")
+a = email[-10]
+b = "@gmail.com"
+c = a in b
+print(c)
